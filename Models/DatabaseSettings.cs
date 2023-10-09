406d71437b312bdf68d4772872d130e836c3eb5f
Vinicius Abreu <vs.abreu@hotmail.com>
2023-10-09 01:11:13 -0300

    Adicionar arquivos de projeto.

---
+++ Models/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreApi.Models
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using StoreApi.Models;
+using StoreApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("Database"));
+
+builder.Services.AddSingleton<BooksService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
